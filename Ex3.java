3f09a0ecc6188207d70a36272986802a755ebf68
LooperCode <savchenkodkz@gmail.com>
2023-04-03 05:40:57 +0600

    upd

--- Ex3.java
+++ Ex3.java
@@ -9,6 +9,7 @@ package JavaHW5;
 
 public class Ex3 {
     public static void getPos(String[][] chsBoard) {
+
         int random = (int) (Math.random() * chsBoard.length);
         chsBoard[0][random] = "X";
         for (int i = 1; i < chsBoard.length; i++) {
@@ -26,19 +27,19 @@ public class Ex3 {
         }
     }
 
-    public static void clearPreStep(String[][] chsBoard, int preRow) {
-        for (int i = 0; i < chsBoard.length; i++) {
+    public static void clearPreStep(String[][] stepBackRow, int preRow) {
 
-            chsBoard[preRow][i] = null;
+        for (int i = 0; i < stepBackRow.length; i++) {
+            stepBackRow[preRow][i] = null;
         }
     }
 
-    public static Boolean checkRight(String[][] chsBoard, int posRight, int index) {
+    public static Boolean checkRight(String[][] rightCol, int posRight, int currentRow) {
 
-        for (int row = index - 1; row >= 0; row--) {
-            if (posRight < chsBoard.length - 1) {
+        for (int row = currentRow - 1; row >= 0; row--) {
+            if (posRight < rightCol.length - 1) {
                 posRight += 1;
-                if (chsBoard[row][posRight] != null) {
+                if (rightCol[row][posRight] != null) {
                     return true;
                 }
             }
@@ -46,12 +47,12 @@ public class Ex3 {
         return false;
     }
 
-    public static Boolean checkLeft(String[][] chsBoard, int posLeft, int index) {
+    public static Boolean checkLeft(String[][] leftCol, int posLeft, int currentRow) {
 
-        for (int row = index - 1; row >= 0; row--) {
+        for (int row = currentRow - 1; row >= 0; row--) {
             if (posLeft > 0) {
                 posLeft -= 1;
-                if (chsBoard[row][posLeft] != null) {
+                if (leftCol[row][posLeft] != null) {
                     return true;
                 }
             }
@@ -59,10 +60,10 @@ public class Ex3 {
         return false;
     }
 
-    public static Boolean checkFront(String[][] chsBoard, int posFront, int index) {
+    public static Boolean checkFront(String[][] frontCol, int posFront, int currentRow) {
 
-        for (int row = index - 1; row >= 0; row--) {
-            if (chsBoard[row][posFront] != null) {
+        for (int row = currentRow - 1; row >= 0; row--) {
+            if (frontCol[row][posFront] != null) {
                 return true;
             }
         }
@@ -70,6 +71,7 @@ public class Ex3 {
     }
 
     public static void printArray(String[][] board) {
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (board[i][j] == null) {
